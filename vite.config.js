55a2e2bf12d8bcdb86498a5cebb39079e5951f01
Wei <lunhowl@gmail.com>
2025-07-08 18:36:26 +0800

    更正 GitHub Pages base 路徑為 Climendar-Vi

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/vue-calendar-app/',
+  base: '/Climendar-Vi/',
   resolve: {
     alias: {
       '@': '/src'
